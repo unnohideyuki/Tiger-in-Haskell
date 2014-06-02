3395fa33a421f3bb694dc71f467dc96edddcca9e
UNNO Hideyuki <unno.hideyuki@jp.fujitsu.com>
2014-06-02 19:30:32 +0900

    Added Symbol.fromString and use it from Temp.

--- chap6/Symbol.hs
+++ chap6/Symbol.hs
@@ -5,6 +5,9 @@ import qualified Data.Map as Map
 type Symbol = String
 type Table = Map.Map Symbol
 
+fromString:: String -> String
+fromString = id
+
 empty = Map.empty
 insert table s v = Map.insert s v table
 lookup table s = Map.lookup s table
--- chap6/Temp.hs
+++ chap6/Temp.hs
@@ -2,5 +2,20 @@ module Temp where
 
 import qualified Symbol
 
-type Temp = Int
+type Temp = (Int, Int)
 type Label = Symbol.Symbol
+
+newTemp :: Temp
+newTemp = undefined
+
+makeString :: Temp -> String
+makeString = undefined
+
+newLabel :: Temp -> Label
+newLabel = undefined
+
+namedLabel :: String -> Label
+namedLabel = Symbol.fromString
+
+
+
