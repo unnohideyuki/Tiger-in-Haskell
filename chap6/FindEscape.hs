3d120ee2b84a765d6167c2bedadc7fad68a61e89
UNNO Hideyuki <unno.hideyuki@jp.fujitsu.com>
2014-06-09 14:19:30 +0900

    temporary commit, writing findExp.

--- chap6/FindEscape.hs
+++ chap6/FindEscape.hs
@@ -57,5 +57,61 @@ findEscape =
       in
        (A.SeqExp exps', fs)
                       
+    findesc exp@A.AssignExp{A.vvar=var, A.exp=e} =
+      let
+        (var', fs1) = findescv var
+        (e', fs2) = findesc e
+      in
+       (exp{A.vvar=var', A.exp=e'}, nub $ fs1 ++ fs2)
+       
+    findesc exp@A.IfExp{A.test=test, A.thene=texp, A.elsee=eexp} =
+      let
+        (test', fs1) = findesc test
+        (texp', fs2) = findesc texp
+        (eexp', fs3) = 
+          case eexp of
+            Just e -> let (e', fs3') = findesc e
+                      in
+                       (Just e', fs3')
+            Nothing -> (Nothing, [])
+      in
+       (exp{A.test=test', A.thene=texp', A.elsee=eexp'},
+        nub $ fs1 ++ fs2 ++ fs3)
+
+
+    findesc exp@A.WhileExp{A.test=test, A.body=body} =
+      let
+        (test', fs1) = findesc test
+        (body', fs2) = findesc body
+      in
+       (exp{A.test=test', A.body=body'}, nub $ fs1 ++ fs2)
+
+    findesc exp@(A.BreakExp _) = (exp, [])
+
+    findesc exp@A.LetExp{A.decs=decs, A.body=body} =
+      let
+        fesc_decs dec (decs, names, fs) = 
+          let
+            (dec', names', fs') = findescd dec
+          in
+           (dec':decs, nub $ names' ++ names, nub $ fs' ++ fs)
+           
+        (decs', names, fs1) = foldr' fesc_decs ([], [], []) decs
+        
+        (body', fs2) = findesc body
+        
+        fs' = [s | s <- (nub $ fs1 ++ fs2), notElem s names]
+      in
+       (exp{A.decs=decs', A.body=body'}, fs')
+
+
+    findescv :: A.Var -> (A.Var, [S.Symbol])
+    findescv = undefined  
+    
+    findescd :: A.Dec -> (A.Dec, [S.Symbol], [S.Symbol])
+    findescd = undefined
+  
+
   in
    findesc
+
