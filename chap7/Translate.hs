406bfe9033c276ad5a79f35a8df220a5d538cff2
unno <unno.hideyuki@nifty.com>
2014-06-11 23:03:26 +0900

    unEx

--- chap7/Translate.hs
+++ chap7/Translate.hs
@@ -5,6 +5,7 @@ module Translate ( Level, Access
 import qualified Temp
 import qualified Frame
 import qualified DalvikFrame as Frame
+import qualified Tree as T
 
 data Level = Level { parent :: Level
                    , name :: Temp.Label
@@ -35,3 +36,33 @@ allocLocal level@Level{frame=frame} escapes temp =
   in
    (Access{level=level, access=access}, level{frame=frame'}, temp')
 
+data Exp = Ex T.Exp
+         | Nx T.Stm
+         | Cx (Temp.Label -> Temp.Label -> T.Stm)
+
+unEx :: Temp.Temp -> Exp -> (T.Exp, Temp.Temp)
+unEx temp = 
+  let
+    unex (Ex e) = (e, temp)
+
+    unex (Cx genstm) =
+      let (r, temp') = Temp.newTemp temp
+          (t, temp'') = Temp.newLabel temp'
+          (f, temp3) = Temp.newLabel temp''
+
+          e = T.ESEQ [T.MOVE (T.TEMP r) (T.CONST 1),
+                      genstm t f,
+                      T.LABEL f,
+                      T.MOVE (T.TEMP r) (T.CONST 0),
+                      T.LABEL t]
+              $ T.TEMP r
+      in
+       (e, temp3)
+       
+    unex (Nx s) = (T.ESEQ [s] $ T.CONST 0, temp)
+  in
+   unex
+
+
+
+    
