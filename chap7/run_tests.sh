7fbab077cca3ed75972743e6aa31ed051c43a499
unno <unno.hideyuki@nifty.com>
2014-07-19 22:29:07 +0900

    before implement strcmp.

--- chap7/run_tests.sh
+++ chap7/run_tests.sh
@@ -4,7 +4,7 @@ cd src/
 alex Lexer.x
 happy Parser.y
 cd ..
-ghc -isrc driver.hs
+ghc -Wall -isrc driver.hs
 
 for f in ../testcases/*.tig ../additonalcases/*.tig ;do 
     echo -n $f ": "; ./driver < $f; done > results.log 2>&1
