89a6fbe350ccf3954d087c189a57346970eac16d
unno <unno.hideyuki@nifty.com>
2014-08-10 22:07:17 +0900

    removed warnings in FindEscape.hs

--- chap9/src/FindEscape.hs
+++ chap9/src/FindEscape.hs
@@ -52,68 +52,68 @@ findEscape =
     findesc (A.SeqExp exps) =
       let
         (exps', fs1, fs2) = foldr'
-                            (\e (es, fs1, fs2) ->
+                            (\e (es, fs01, fs02) ->
                               let
-                                (e', fs1', fs2') = findesc e
+                                (e', fs01', fs02') = findesc e
                               in
-                               (e':es, nub$fs1'++fs1, nub$fs2'++fs2))
+                               (e':es, nub$fs01'++fs01, nub$fs02'++fs02))
                             ([], [], [])
                             exps
       in
        (A.SeqExp exps', fs1, fs2)
                       
-    findesc exp@A.AssignExp{A.vvar=var, A.exp=e} =
+    findesc exp'@A.AssignExp{A.vvar=var, A.exp=e} =
       let
         (var', fs1, fs2) = findescv var
         (e', fs1', fs2') = findesc e
       in
-       (exp{A.vvar=var', A.exp=e'}, nub$fs1'++fs1, nub$fs2'++fs2)
+       (exp'{A.vvar=var', A.exp=e'}, nub$fs1'++fs1, nub$fs2'++fs2)
        
-    findesc exp@A.IfExp{A.test=test, A.thene=texp, A.elsee=eexp} =
+    findesc exp'@A.IfExp{A.test=test, A.thene=texp, A.elsee=eexp} =
       let
         (test', fs1, fs2) = findesc test
         (texp', fs1', fs2') = findesc texp
         (eexp', fs1'', fs2'') = 
           case eexp of
-            Just e -> let (e', fs1, fs2) = findesc e
+            Just e -> let (e', fs01, fs02) = findesc e
                       in
-                       (Just e', fs1, fs2)
+                       (Just e', fs01, fs02)
             Nothing -> (Nothing, [], [])
       in
-       (exp{A.test=test', A.thene=texp', A.elsee=eexp'},
+       (exp'{A.test=test', A.thene=texp', A.elsee=eexp'},
         nub $ fs1 ++ fs1'++ fs1'',
         nub $ fs2 ++ fs2'++ fs2'')
 
-    findesc exp@A.WhileExp{A.test=test, A.body=body} =
+    findesc exp'@A.WhileExp{A.test=test, A.body=body} =
       let
         (test', fs1, fs2) = findesc test
         (body', fs1', fs2') = findesc body
       in
-       (exp{A.test=test', A.body=body'}, nub$fs1++fs1',nub$fs2++fs2')
+       (exp'{A.test=test', A.body=body'}, nub$fs1++fs1',nub$fs2++fs2')
 
-    findesc exp@(A.BreakExp _) = (exp, [], [])
+    findesc exp'@(A.BreakExp _) = (exp', [], [])
 
-    findesc exp@A.LetExp{A.decs=decs, A.body=body} =
+    findesc exp'@A.LetExp{A.decs=decs0, A.body=body} =
       let
-        fesc_decs dec (decs, fs1, fs2) = 
+        fesc_decs dec (decs, fs01, fs02) = 
           let
-            (dec', fs1', fs2') = findescd dec fs1 fs2
+            (dec', fs01', fs02') = findescd dec fs01 fs02
           in
-           (dec':decs, nub$fs1'++fs1, nub$fs2'++fs2)
+           (dec':decs, nub$fs01'++fs01, nub$fs02'++fs02)
            
         (body', fs1b, fs2b) = findesc body
-        (decs', fs1, fs2) = foldr' fesc_decs ([], fs1b, fs2b) decs
+        (decs', fs1, fs2) = foldr' fesc_decs ([], fs1b, fs2b) decs0
       in
-       (exp{A.decs=decs', A.body=body'}, fs1, fs2)
+       (exp'{A.decs=decs', A.body=body'}, fs1, fs2)
 
-    findesc exp@A.ArrayExp{A.size=size, A.init=init} =
+    findesc exp'@A.ArrayExp{A.size=size, A.init=init0} =
       let
         (size', fs1, fs2) = findesc size
-        (init', fs1', fs2') = findesc init
+        (init', fs1', fs2') = findesc init0
       in
-       (exp{A.size=size', A.init=init'}, nub$fs1++fs1', nub$fs2++fs2')
+       (exp'{A.size=size', A.init=init'}, nub$fs1++fs1', nub$fs2++fs2')
 
-    findesc exp@A.ForExp {A.svar=svar, A.lo=lo, A.hi=hi, A.body=body} =
+    findesc exp'@A.ForExp {A.svar=svar, A.lo=lo, A.hi=hi, A.body=body} =
       let
         (lo', fs1, fs2) = findesc lo
         (hi', fs1', fs2') = findesc hi
@@ -124,21 +124,21 @@ findEscape =
         
         esc = or [True | s<-fs2'', s == svar]
       in
-       (exp{A.lo=lo', A.hi=hi', A.body=body', A.escape=esc}, 
+       (exp'{A.lo=lo', A.hi=hi', A.body=body', A.escape=esc}, 
         nub $ fs1 ++ fs1' ++ fs13, nub $ fs2 ++ fs2' ++ fs23)
     
-    findesc exp@A.CallExp{A.func=func, A.args=args} = 
+    findesc exp'@A.CallExp{A.func=func, A.args=args} = 
       let
         (args', fs1', fs2') = foldr'
-                              (\arg (args, fs1, fs2) ->
+                              (\arg (args1, fs01, fs02) ->
                                 let
-                                  (arg', fs1', fs2') = findesc arg
+                                  (arg', fs01', fs02') = findesc arg
                                 in
-                                 (arg':args, nub$fs1'++fs1, nub$fs2'++fs2))
+                                 (arg':args1, nub$fs01'++fs01, nub$fs02'++fs02))
                               ([], [], [])
                               args
       in
-       (exp{A.args=args'}, nub $func:fs1', fs2')
+       (exp'{A.args=args'}, nub $func:fs1', fs2')
 
 
     findescv :: A.Var -> (A.Var, [S.Symbol], [S.Symbol])
@@ -151,19 +151,19 @@ findEscape =
       in
        (A.FieldVar var' sym pos, fs1, fs2)
        
-    findescv (A.SubscriptVar var exp pos) =
+    findescv (A.SubscriptVar var exp0 pos) =
       let
         (var', fs1, fs2) = findescv var
-        (exp', fs1', fs2') = findesc exp
+        (exp', fs1', fs2') = findesc exp0
       in
        (A.SubscriptVar var' exp' pos, nub$fs1++fs1', nub$fs2++fs2')
     
     findescd :: A.Dec -> [S.Symbol] -> [S.Symbol]
                 -> (A.Dec, [S.Symbol], [S.Symbol])
     
-    findescd d@A.VarDec{A.name'=name, A.init'=init} fs1 fs2 =
+    findescd d@A.VarDec{A.name'=name, A.init'=init0} fs1 fs2 =
       let
-        (init', fs1_ini, fs2_ini) = findesc init
+        (init', fs1_ini, fs2_ini) = findesc init0
         
         fs1' = [s | s <- fs1, s /= name]
         fs2' = [s | s <- fs2, s /= name]
@@ -175,7 +175,7 @@ findEscape =
 
     findescd d@(A.TypeDec _) fs1 fs2 = (d, fs1, fs2)
     
-    findescd (A.FunctionDec fundecs) fs1 fs2 =
+    findescd (A.FunctionDec fundecs) fs01 fs02 =
       let
         funfs fdec@A.FuncDec {A.params=params, A.func_body=body} (fdecs, fs) =
           let
@@ -183,11 +183,11 @@ findEscape =
             fs_bd = nub $ fs1 ++ fs2
             
             params' = foldr'
-                      (\f@A.Field{A.field_name=name} params ->
+                      (\f@A.Field{A.field_name=name} params1 ->
                         if elem name fs2 then
-                          f{A.field_esc=True}:params
+                          f{A.field_esc=True}:params1
                         else
-                          f{A.field_esc=False}:params)
+                          f{A.field_esc=False}:params1)
                       []
                       params
                       
@@ -198,6 +198,6 @@ findEscape =
         
         (fundecs', fs_body) = foldr funfs ([], []) fundecs
       in
-       (A.FunctionDec fundecs', fs1, nub $ fs_body ++ fs2)
+       (A.FunctionDec fundecs', fs01, nub $ fs_body ++ fs02)
   in
    findesc
