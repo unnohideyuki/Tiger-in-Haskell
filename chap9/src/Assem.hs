22a1ac9b0acb59a2c834f281d7fcac6a37b2d313
unno <unno.hideyuki@nifty.com>
2014-09-04 00:42:42 +0900

    const and binops.

--- chap9/src/Assem.hs
+++ chap9/src/Assem.hs
@@ -51,7 +51,18 @@ binOper :: String -> [Int] -> [Int] -> Instr
 binOper binop dst src =
   let
     assem ds ss _ =
-      binop ++ " " ++ (ds!!0) ++ ", " ++ (ss!!0) ++ ", " ++ (ss!!1)
+      let
+        s1 = "check-cast " ++ (ss!!0) ++ ", Ljava/lang/Integer;\n"
+        s2 = "check-cast " ++ (ss!!1) ++ ", Ljava/lang/Integer;\n"
+        s3 = "invoke-virtual {" ++ (ss!!0) ++ "}, Ljava/lang/Integer;.intValue:()I\n"
+        s4 = "move-result " ++ (ds!!1) ++ "\n"
+        s5 = "invoke-virtual {" ++ (ss!!1) ++ "}, Ljava/lang/Integer;.intValue:()I\n"
+        s6 = "move-result " ++ (ds!!2) ++ "\n"
+        s7 = binop ++ " " ++ (ds!!1) ++ ", " ++ (ds!!1) ++ ", " ++ (ds!!2) ++ "\n"
+        s8 = "new-instance " ++ (ds!!0) ++ ", Ljava/lang/Integer;\n"
+        s9 = "invoke-direct {" ++ (ds!!0) ++ ", " ++ (ds!!1) ++ ", Ljava/lang/Integer;.<init>:(I)V"
+      in
+       concat [s1, s2, s3, s4, s5, s6, s7, s8, s9]
   in
    OPER { oper_assem = assem
         , oper_dst = dst
@@ -71,15 +82,20 @@ mulInstr = binOper "mul-int"
 divInstr :: [Int] -> [Int] -> Instr
 divInstr = binOper "div-int"
 
-constInstr :: Int -> Int -> Instr
+constInstr :: Int -> [Int] -> Instr
 constInstr c dst =
   let
     assem ds _ _ =
-      "const " ++ (ds!!0) ++ ", #+" ++ (show c)
-      -- TODO: the immediate value should be hex?
+      let
+        i1 = "const " ++ (ds!!1) ++ ", #int " ++ (show c)
+        i2 = "new-instance " ++ (ds!!0) ++ ", Ljava/lang/Integer;"
+        i3 = "invoke-direct {" ++ (ds!!0) ++ ", " ++ (ds!!1) 
+             ++ "}, Ljava/lang/Integer;.<init>:(I)V"
+      in
+       concat [i1, "\n", i2, "\n", i3]
   in
    OPER { oper_assem = assem
-        , oper_dst = [dst]
+        , oper_dst = dst
         , oper_src = []
         , oper_jump = Nothing
         }
