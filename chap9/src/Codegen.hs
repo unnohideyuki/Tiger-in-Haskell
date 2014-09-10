b4f697ba6cd179c36f01c7cda83565f8de48e6aa
unno <unno.hideyuki@nifty.com>
2014-09-11 01:09:34 +0900

    refactor the State Monad.

--- chap9/src/Codegen.hs
+++ chap9/src/Codegen.hs
@@ -7,31 +7,45 @@ import qualified Tree as T
 import qualified DalvikFrame as F
 import qualified Temp
 
-data InstsTemp = InstsTemp { get_insts :: [A.Instr]
+data CgenState = CgenState { get_insts :: [A.Instr]
                            , get_temp :: Temp.Temp 
                            , get_frame :: F.Frame }
 
-munchExp :: T.Exp -> State InstsTemp Int
+-- Methods of the State Monad
+
+newTemp :: State CgenState Int
+newTemp = state $ \st ->
+  let
+    temp = get_temp st
+    (t, temp') = Temp.newTemp temp
+  in
+   (t, st{get_temp=temp'})
+   
+emit :: A.Instr -> State CgenState ()
+emit inst = state $ \st ->
+  let
+    insts = get_insts st
+  in
+   ((), st{get_insts=insts++[inst]})
+   
+get_arcd :: State CgenState Int
+get_arcd = state $ \st -> (F.arcd $ get_frame st, st)
+
+-- munchExp
+
+munchExp :: T.Exp -> State CgenState Int
 
 munchExp (T.CONST n) = 
-  state $ \st -> 
-  let
-    insts = get_insts st
-    temp = get_temp st
-    (t, temp') = Temp.newTemp temp
-    inst = A.constInstr n [t]
-  in
-   (t, st{get_insts=insts ++ [inst], get_temp=temp'})
+  do
+    t <- newTemp
+    emit $ A.constInstr n [t]
+    return t
    
 munchExp (T.STR s) =
-  state $ \st ->
-  let
-    insts = get_insts st
-    temp = get_temp st
-    (t, temp') = Temp.newTemp temp
-    inst = A.strInstr s [t]
-  in
-   (t, st{get_insts=insts ++ [inst], get_temp=temp'})
+  do
+    t <- newTemp
+    emit $ A.strInstr s [t]
+    return t
     
 munchExp (T.TEMP t) = return t
 
@@ -43,49 +57,28 @@ munchExp (T.BINOP op e1 e2) =
       T.MUL -> A.mulInstr
       T.DIV -> A.divInstr
       _ -> error "not support binInstr"
-    
-    munchBinop src =
-      state $ \st ->
-      let
-        insts = get_insts st
-        temp = get_temp st
-        (d0, temp1) = Temp.newTemp temp
-        (d1, temp2) = Temp.newTemp temp1
-        (d2, temp3) = Temp.newTemp temp2
-        inst = binInstr [d0, d1, d2] src
-      in
-       (d0, st{get_insts=(insts ++ [inst]), get_temp=temp3})
   in
    do
      t1 <- munchExp e1
      t2 <- munchExp e2
-     munchBinop [t1, t2]
+     d0 <- newTemp
+     d1 <- newTemp
+     d2 <- newTemp
+     emit $ binInstr [d0, d1, d2] [t1, t2]
+     return d0
     
 munchExp (T.MEM e) =
-  let
-    get_arcd = state $ \st -> (F.arcd $ get_frame st, st)
-  in
-   do
-     t <- get_arcd
-     munchExp (T.ARR (T.TEMP t) e)
+  do
+    t <- get_arcd
+    munchExp (T.ARR (T.TEMP t) e)
 
 munchExp (T.ARR e1 e2) =
-  let
-    munchArr src =
-      state $ \st ->
-      let
-        insts = get_insts st
-        temp = get_temp st
-        (d0, temp') = Temp.newTemp temp
-        (d1, temp'') = Temp.newTemp temp'
-        inst = A.memInstr [d0, d1] src
-      in
-       (d0, st{get_insts=insts ++ [inst], get_temp=temp''})
-  in
-   do
-     s1 <- munchExp e1
-     s2 <- munchExp e2
-     munchArr [s1, s2]
+  do
+    s0 <- munchExp e1
+    s1 <- munchExp e2
+    d0 <- newTemp
+    d1 <- newTemp
+    emit $ A.memInstr [d0, d1] [s0, s1]
+    return d0
   
 munchExp (T.RCD e i) = munchExp (T.ARR e (T.CONST i))
-
