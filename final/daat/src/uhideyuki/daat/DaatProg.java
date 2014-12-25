8baad4be2b17909e67f45d149566244c05fc7a34
UNNO Hideyuki <unno.hideyuki@nifty.com>
2014-12-25 22:17:24 +0900

    Daat Sample

---
+++ final/daat/src/uhideyuki/daat/DaatProg.java
@@ -0,0 +1,11 @@
+package uhideyuki.daat;
+
+import uhideyuki.daat.DaatRuntime;
+
+public class DaatProg
+{
+    public static void main()
+    {
+        DaatRuntime.print("Hello, world!\n");
+    }
+}
--- final/daat/src/uhideyuki/daat/DaatRuntime.java
+++ final/daat/src/uhideyuki/daat/DaatRuntime.java
@@ -2,14 +2,26 @@ package uhideyuki.daat;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import uhideyuki.daat.R.id;
+import uhideyuki.daat.DaatProg;
 
 public class DaatRuntime extends Activity
 {
-    /** Called when the activity is first created. */
+    static TextView tv;
+
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        tv = (TextView) findViewById(id.tv_main);
+        DaatProg.main();
+    }
+
+    public static void print(String s)
+    {
+        tv.append(s);
     }
 }
