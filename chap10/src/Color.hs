08a4c0f63a85eaa11f4af2f8ff04f7444cfa2663
UNNO Hideyuki <unno.hideyuki@nifty.com>
2015-01-09 01:13:22 +0900

    Color.hs and ColorSpec.hs.

--- chap10/src/Color.hs
+++ chap10/src/Color.hs
@@ -87,8 +87,82 @@ build nodes defMap useMap isMoveMap liveOutMap = fst $ runState (build' nodes) n
       in
        ((), st{adjSet=aset, adjList=alist})
 
+data AssignColorStatus =  AssignColorStatus
+                          { selectStack :: [Int]
+                          , colorMap :: Map.Map Int Int
+                          }
 
-        
+assignColor :: Interference -> [Int] -> Map.Map Int Int 
+assignColor adjListMap selectStack0 = cmap
+  where
+    k = 14 -- number of colors (registers)
 
+    adjlist n = case Map.lookup n adjListMap of
+      Just xs -> xs
+      _ -> []
 
-          
+    newStatus stack = AssignColorStatus stack Map.empty
+
+    assignLoop :: State AssignColorStatus (Map.Map Int Int)
+    assignLoop = do
+      n <- pop
+      adjNodes <- adjacency n
+      okColors <- calcOkColors adjNodes [0..(k-1)]
+      colorNode n okColors
+      sz <- stackSize
+      if sz > 0 then assignLoop else retColMap
+
+    pop :: State AssignColorStatus Int
+    pop = state $ \st ->
+      let
+        stack = selectStack st
+        stack' = tail stack
+        n = head stack
+      in
+       (n, st{selectStack=stack'})
+
+    adjacency :: Int -> State AssignColorStatus [Int]
+    adjacency n = state $ \st ->
+      let
+        stack = selectStack st
+      in
+       (adjlist n \\ stack, st)
+
+    calcOkColors :: [Int] -> [Int] -> State AssignColorStatus [Int]
+    calcOkColors [] cols = return cols
+    calcOkColors (w:ws) cols = do
+      cols' <- checkcol w cols
+      calcOkColors ws cols'
+
+    checkcol :: Int -> [Int] -> State AssignColorStatus [Int]
+    checkcol w cols = state $ \st ->
+      let
+        colmap = colorMap st
+        cols' = case Map.lookup w colmap of
+          Just c -> delete c cols
+          _ -> cols
+      in
+       (cols', st)
+
+    colorNode :: Int -> [Int] -> State AssignColorStatus ()
+    colorNode _ [] = error "spilt!"
+    colorNode n cols = state $ \st ->
+      let
+        c = head cols
+        colmap = colorMap st
+        colmap' = Map.insert n c colmap
+      in
+       ((), st{colorMap=colmap'})
+
+    stackSize :: State AssignColorStatus Int
+    stackSize = state $ \st ->
+      let
+        stack = selectStack st
+      in
+       (length stack, st)
+
+    retColMap :: State AssignColorStatus (Map.Map Int Int)
+    retColMap = state $ \st -> (colorMap st, st)
+
+    (cmap, _) = runState assignLoop $ newStatus selectStack0
+    
